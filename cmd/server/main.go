c00335bc9714140c2ee8e31b2171362327aad315
Lucas Gomes <lucasdavip@hotmail.com>
2024-10-17 21:40:48 -0300

    Init config rate limit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,16 +2,43 @@ package main
 
 import (
 	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-redis/redis/v8"
+	"github.com/lucasdpg/rate-limiter/internal/store"
+	"github.com/lucasdpg/rate-limiter/pkg/limiter"
 )
 
 func main() {
+	// Inicializando o cliente Redis
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
 
+	// Criando uma instância do RedisStore
+	redisStore := store.NewRedisStore(rdb)
+
+	// Criando uma instância do Rate Limiter
+	rl := limiter.NewRateLimiter(redisStore, 10, 20, time.Minute*1)
+
+	// Inicializando o Chi Router
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	// Aplicando o middleware de Rate Limiting
+	r.Use(limiter.RateLimitMiddleware(rl))
+
+	// Servindo arquivos estáticos
 	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	r.Handle("/static/*", http.StripPrefix("/static/", fs))
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// Servindo a página HTML
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "static/index.html")
 	})
 
-	http.ListenAndServe(":8080", nil)
+	// Iniciando o servidor HTTP
+	http.ListenAndServe(":8080", r)
 }
--- pkg/limiter/limiter_middleware.go
+++ pkg/limiter/limiter_middleware.go
@@ -1,3 +1,39 @@
 package limiter
 
-// Middleware para integrar o rate limiter
+import (
+	"net/http"
+)
+
+// RateLimitMiddleware é um middleware para aplicar o rate limiter
+func RateLimitMiddleware(rl *RateLimiter) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ip := r.RemoteAddr
+			token := r.Header.Get("API_KEY")
+
+			if token != "" {
+				exceeded, err := rl.CheckRateLimitToken(token)
+				if err != nil {
+					http.Error(w, "Error checking the limit", http.StatusInternalServerError)
+					return
+				}
+				if exceeded {
+					http.Error(w, "you have reached the maximum number of requests or actions allowed within a certain time frame", http.StatusTooManyRequests)
+					return
+				}
+			} else {
+				exceeded, err := rl.CheckRateLimitIP(ip)
+				if err != nil {
+					http.Error(w, "Error checking the limit", http.StatusInternalServerError)
+					return
+				}
+				if exceeded {
+					http.Error(w, "you have reached the maximum number of requests or actions allowed within a certain time frame", http.StatusTooManyRequests)
+					return
+				}
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
