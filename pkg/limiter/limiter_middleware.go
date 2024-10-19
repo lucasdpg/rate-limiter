ca12646d8ba8afebcc39fc757d038a0b01ac472a
Lucas Gomes <lucasdavip@hotmail.com>
2024-10-19 10:55:29 -0300

    fix ip check

--- pkg/limiter/limiter_middleware.go
+++ pkg/limiter/limiter_middleware.go
@@ -1,13 +1,18 @@
 package limiter
 
 import (
+	"net"
 	"net/http"
 )
 
 func RateLimitMiddleware(rl *RateLimiter) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ip := r.RemoteAddr
+			ip, _, err := net.SplitHostPort(r.RemoteAddr)
+			if err != nil {
+				http.Error(w, "Unable to parse IP address", http.StatusInternalServerError)
+				return
+			}
 			token := r.Header.Get("API_KEY")
 
 			if token != "" {
